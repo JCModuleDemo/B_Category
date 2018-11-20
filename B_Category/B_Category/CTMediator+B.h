2837c298489c7cd4198b9cc08ea9bda2d04031b7
JuneCheng <755038283@qq.com>
2018-11-20 14:26:13 +0800

    1

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,19 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by JuneCheng on 2018/11/20.
+//  Copyright © 2018 zjhcsoft. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ B_Category/B_Category/CTMediator+B.m
@@ -0,0 +1,23 @@
+//
+//  CTMediator+B.m
+//  B_Category
+//
+//  Created by JuneCheng on 2018/11/20.
+//  Copyright © 2018 zjhcsoft. All rights reserved.
+//
+
+#import "CTMediator+B.h"
+
+@implementation CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText {
+    
+    /*
+     BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
+     */
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"contentText"] = contentText;
+    return [self performTarget:@"B" action:@"viewController" params:params shouldCacheTarget:NO];
+}
+
+@end
